7c2434de1dda38edc9334998ecfa04778a454aec
DivyanshGoel20 <divyanshgoel17@gmail.com>
2024-03-29 21:24:58 +0530

    add: Execute feature UI

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,32 +1,11 @@
 "use client";
 
 import React, { useState } from "react";
-import Link from "next/link";
 import type { NextPage } from "next";
-// import { useAccount } from "wagmi";
 import * as chains from "wagmi/chains";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-
-// const [selectedNetwork, setSelectedNetwork] = React.useState("goerli");
 
 const Home: NextPage = () => {
-  // const chains_dict = {
-  //   homestead: "eth-mainnet",
-  //   goerli: "eth-goerli",
-  //   matic: "matic-mainnet",
-  //   gnosis: "gnosis-mainnet",
-  //   bsc: "bsc-mainnet",
-  //   arbitrum: "arbitrum-mainnet",
-  //   optimism: "optimism-mainnet",
-  //   avalanche: "avalanche-mainnet",
-  //   aurora: "aurora-mainnet",
-  //   base: "base-mainnet",
-  //   "base-goerli": "base-testnet",
-  //   "polygon-zkevm": "polygon-zkevm-mainnet",
-  //   "zksync-era": "zksync-mainnet",
-  // };
-
   const networks = [
     chains.mainnet,
     chains.goerli,
@@ -43,7 +22,6 @@ const Home: NextPage = () => {
     chains.zkSync,
   ];
 
-  // const { address: connectedAddress } = useAccount();
   const [inputValue, setInputValue] = useState("");
   const [selectedNetwork, setSelectedNetwork] = React.useState(chains["goerli"]);
 
@@ -60,9 +38,8 @@ const Home: NextPage = () => {
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
-            <span className="block text-4xl mt-32 font-bold">Tiny DEX</span>
+            <span className="block text-4xl mb-32 mt-32 font-bold">Tiny DEX</span>
           </h1>
-          <p className="py-8 text-center">No Chickfill-a Sauce !!</p>
         </div>
 
         <div className="flex items-center mb-1">
@@ -159,31 +136,6 @@ const Home: NextPage = () => {
         <button className="btn btn-primary w-1/6" onClick={() => dummy()}>
           Swap
         </button>
-
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-          </div>
-        </div>
       </div>
     </>
   );
