f8725990a9f2090f095c7f71b97886bb83078639
DivyanshGoel20 <divyanshgoel17@gmail.com>
2024-03-29 19:55:31 +0530

    add: Swap feature UI

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,43 +1,164 @@
 "use client";
 
+import React, { useState } from "react";
 import Link from "next/link";
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
+// import { useAccount } from "wagmi";
+import * as chains from "wagmi/chains";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
+
+// const [selectedNetwork, setSelectedNetwork] = React.useState("goerli");
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
+  // const chains_dict = {
+  //   homestead: "eth-mainnet",
+  //   goerli: "eth-goerli",
+  //   matic: "matic-mainnet",
+  //   gnosis: "gnosis-mainnet",
+  //   bsc: "bsc-mainnet",
+  //   arbitrum: "arbitrum-mainnet",
+  //   optimism: "optimism-mainnet",
+  //   avalanche: "avalanche-mainnet",
+  //   aurora: "aurora-mainnet",
+  //   base: "base-mainnet",
+  //   "base-goerli": "base-testnet",
+  //   "polygon-zkevm": "polygon-zkevm-mainnet",
+  //   "zksync-era": "zksync-mainnet",
+  // };
+
+  const networks = [
+    chains.mainnet,
+    chains.goerli,
+    chains.polygon,
+    chains.gnosis,
+    chains.bsc,
+    chains.arbitrum,
+    chains.optimism,
+    chains.avalanche,
+    chains.aurora,
+    chains.base,
+    chains.baseGoerli,
+    chains.polygonZkEvm,
+    chains.zkSync,
+  ];
+
+  // const { address: connectedAddress } = useAccount();
+  const [inputValue, setInputValue] = useState("");
+  const [selectedNetwork, setSelectedNetwork] = React.useState(chains["goerli"]);
+
+  const handleInputChange = (e: any) => {
+    setInputValue(e.target.value);
+  };
+
+  function dummy() {
+    console.log("dummy");
+  }
 
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl mt-32 font-bold">Tiny DEX</span>
           </h1>
-          <div className="flex justify-center items-center space-x-2">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
-          </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
+          <p className="py-8 text-center">No Chickfill-a Sauce !!</p>
         </div>
+
+        <div className="flex items-center mb-1">
+          <div className="flex-none w-2/1 mr-4">
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              style={{
+                border: "2px solid black",
+                padding: "8px",
+                borderRadius: "10px",
+                width: "100%",
+              }}
+            />
+          </div>
+          <div className="dropdown dropdown-bottom w-2/6 flex-none">
+            <label tabIndex={0} className="btn btn-neutral btn-md dropdown-toggle gap-1">
+              <span>{selectedNetwork.name} </span> <ChevronDownIcon className="h-6 w-4 ml-2 sm:ml-0" />
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu p-2 mt-1 shadow-center shadow-accent bg-base-200 rounded-box gap-1"
+              style={{ maxHeight: "200px", overflowY: "auto" }}
+            >
+              {networks.map((option: any) => (
+                <li key={option.name}>
+                  <button
+                    className="btn-sm !rounded-xl flex py-3 gap-6"
+                    type="button"
+                    onClick={() => setSelectedNetwork(option)}
+                  >
+                    {option.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#222222"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <line x1="12" y1="5" x2="12" y2="19"></line>
+          <polyline points="19 12 12 19 5 12"></polyline>
+        </svg>
+
+        <div className="flex items-center mb-9">
+          <div className="flex-none w-2/1 mr-4">
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              style={{
+                border: "2px solid black",
+                padding: "8px",
+                borderRadius: "10px",
+                width: "100%",
+              }}
+            />
+          </div>
+          <div className="dropdown dropdown-bottom w-2/6 flex-none">
+            <label tabIndex={0} className="btn btn-neutral btn-md dropdown-toggle gap-1">
+              <span>{selectedNetwork.name} </span> <ChevronDownIcon className="h-6 w-4 ml-2 sm:ml-0" />
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu p-2 mt-1 shadow-center shadow-accent bg-base-200 rounded-box gap-1"
+              style={{ maxHeight: "200px", overflowY: "auto" }}
+            >
+              {networks.map((option: any) => (
+                <li key={option.name}>
+                  <button
+                    className="btn-sm !rounded-xl flex py-3 gap-6"
+                    type="button"
+                    onClick={() => setSelectedNetwork(option)}
+                  >
+                    {option.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <button className="btn btn-primary w-1/6" onClick={() => dummy()}>
+          Swap
+        </button>
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
